4aef34fa0dc057788215ec85a683e8b2e22357be
Lukas Mahr <lukas@yousuckatprogramming.de>
2022-12-10 12:27:40 +0100

    Add test for Async Api

--- mvg_api/tests/api_tests.py
+++ mvg_api/tests/api_tests.py
@@ -1,7 +1,8 @@
+import asyncio
 import datetime
 from typing import List
 
-from mvg_api.api.api import Api
+from mvg_api.api.api import Api, AsyncApi
 from mvg_api.mvg import LocationNotFound
 from mvg_api.models.route import LocationList, LocationType, Connections
 from mvg_api.models.ticker import TickerList, SlimList
@@ -50,6 +51,59 @@ def test_get_route():
 def test_get_location():
     api = Api()
     response = api.get_location("Marienplatz")
+    assert isinstance(response, LocationList)
+    for location in response.locations:
+        if location.type == LocationType.STATION:
+            if location.name == "Marienplatz":
+                return
+    raise LocationNotFound("Loaction Marienplatz not foudn in LocationList")
+
+
+def test_get_ticker_async():
+    api = AsyncApi()
+    response = asyncio.run(api.get_ticker())
+    assert isinstance(response, TickerList)
+
+
+def test_get_current_date_async():
+    api = AsyncApi()
+    response = asyncio.run(api.get_current_date())
+    assert isinstance(response, datetime.datetime)
+
+
+def test_get_station_global_ids_async():
+    api = AsyncApi()
+    response = asyncio.run(api.get_station_global_ids())
+    assert isinstance(response, List)
+    assert len(response) > 0
+
+
+def test_get_slim_async():
+    api = AsyncApi()
+    response = asyncio.run(api.get_slim())
+    assert isinstance(response, SlimList)
+
+
+def test_get_route_async():
+    api = AsyncApi()
+    response = asyncio.run(
+        api.get_route(
+            "de:09162:6",
+            "de:09162:50",
+            sap_tickets=True,
+            _time=datetime.datetime.now(),
+            transport_type_call_taxi=True,
+            max_walk_time_to_dest=5,
+            max_walk_time_to_start=5,
+            change_limit=3,
+        )
+    )
+    assert isinstance(response, Connections)
+
+
+def test_get_location_async():
+    api = AsyncApi()
+    response = asyncio.run(api.get_location("Marienplatz"))
     assert isinstance(response, LocationList)
     for location in response.locations:
         if location.type == LocationType.STATION:
