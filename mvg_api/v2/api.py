0d33b52362ac4cf012d22a19b8e667df3647a891
Lukas Mahr <lukas@yousuckatprogramming.de>
2023-07-07 12:41:00 +0200

    Fix ittering over None object for locationTypes

--- mvg_api/v2/api.py
+++ mvg_api/v2/api.py
@@ -104,7 +104,7 @@ class MVGRequests:
                 "query": query,
                 "limitAddressPoi": limit_address_poi,
                 "limitStations": limit_stations,
-                "locationTypes": ",".join([x.value for x in location_types]),
+                "locationTypes": ",".join([x.value for x in location_types]) if location_types is not None else None,
             }
         )
         return httpx.Request(
