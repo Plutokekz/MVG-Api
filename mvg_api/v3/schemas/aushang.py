a3ffe2c913f84a4880e7834f2b3c741b31633109
Lorenz Lehle <lorenz.lehle@proton.me>
2026-02-17 23:39:10 +0100

    aushang: enum for schedulekind

--- mvg_api/v3/schemas/aushang.py
+++ mvg_api/v3/schemas/aushang.py
@@ -1,24 +1,44 @@
 from __future__ import annotations
 
-from typing import List
+from enum import Enum
+from typing import List, Union
+from pydantic import BaseModel, RootModel, field_validator
 
-from pydantic import BaseModel, RootModel
+from mvg_api.v3.schemas import create_flexible_enum_validator
+
+
+class AushangScheduleKind(Enum):
+    STATION_OVERVIEW_MAP = "STATION_OVERVIEW_MAP"
+    """general overview map of a station, and nearby streets and institutions"""
+    CONTEXT_MAP = "CONTEXT_MAP"
+    """shows the different stop points of e.g. busses or tram lines"""
+    SUBWAY = "SUBWAY"
+    TRAM = "TRAM"
+    BUS = "BUS"
+    NIGHT_LINE = "NIGHT_LINE"
+
+    def is_station_overview_map(self):
+        """Returns true if the aushang is the larger station overview map."""
+        return self == AushangScheduleKind.STATION_OVERVIEW_MAP
+
+    def is_context_map(self):
+        """Returns true if the aushang is the stop point context map."""
+        return self == AushangScheduleKind.CONTEXT_MAP
 
 
 class Aushang(BaseModel):
     """A single aushang pdf document like a map or timetable"""
     uri: str
     """The URI to the pdf document, typically hosted at mvg.de"""
-    scheduleKind: str
-    """
-    Type of aushang; a transport type or the constants
-    * STATION_OVERVIEW_MAP for the general overview map of a station, and nearby streets and institutions
-    * CONTEXT_MAP that shows the different stop points of e.g. busses or tram lines
-    """
+    scheduleKind: Union[AushangScheduleKind, str]
+    """Type of aushang; a transport type or aushang specific constants"""
     scheduleName: str
-    """The line number in context with the transport type"""
+    """The line number in context with the transport type in the schedule kind"""
     direction: str
-    """Textual description of the aushang; might include validity information for timetables during e.g. construction work"""
+    """Nicetext description of the aushang; might include validity information for timetables during e.g. construction work"""
+
+    # flexible validators such that pydantic does not fail if a value that is not in the enum is encountered
+    _validate_scheduleKind = field_validator('scheduleKind', mode='before')(create_flexible_enum_validator(AushangScheduleKind))
 
 
 class Aushaenge(RootModel):
